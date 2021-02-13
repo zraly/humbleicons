3eed7ff7328d87d9d75502c4acb33ea1710257d7
Jiri Zraly <jirka@blueboard.cz>
2021-02-13 02:37:11 +0100

    Humbleicons.php

---
+++ src/Humbleicons.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright (c) 2021 Jiří Zralý
+ */
+
+declare(strict_types=1);
+
+namespace Humbleicons;
+
+class Humbleicons {
+
+	private array $icons;
+
+	private int $size;
+
+	private const ICON_PATH = '/icons/';
+
+	public function __construct()
+	{
+		$this->size = 24;
+	}
+
+	public function icon(string $iconName): string
+	{
+		$iconHTML = @file_get_contents(__DIR__ . self::ICON_PATH . $this->size . '/' . $iconName . '.svg');
+		if ($iconHTML === false) {
+			return '';
+		}
+		return $iconHTML;
+	}
+}
